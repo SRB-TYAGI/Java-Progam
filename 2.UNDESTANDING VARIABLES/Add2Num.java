3e2a3fb0d849fd03098659807b0e8cbdd706ea19
Sourabh Tyagi <131552707+SRB-TYAGI@users.noreply.github.com>
2024-03-12 15:04:54 +0530

    Add files via upload

---
+++ 2.UNDESTANDING VARIABLES/Add2Num.java
@@ -0,0 +1,28 @@
+public class Add2Num {
+
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 15;
+		
+		int k = i + j;
+		System.out.println(k);
+		
+		i = 13;
+		
+		
+		short s = 123;
+		
+		long l = 12345;
+		
+		double d = 1.23;
+		
+		char c = 'a';
+		
+		boolean b = true;
+		boolean otherB = false;
+		
+		//float f;
+		//System.out.println(f);
+	}
+
+}
