61eb3ccb38cccfd6b553fd793ca93a3369d6e245
Sourabh Tyagi <131552707+SRB-TYAGI@users.noreply.github.com>
2024-03-16 00:27:51 +0530

    How to store data

---
+++ How to store data.java
@@ -0,0 +1,25 @@
+public class DataType {
+
+	public static void main(String[] args) {
+		char c = 'a';
+		System.out.println(c + 3);
+		
+		int i = c + 10;
+		System.out.println(i);
+		
+		i = c;
+//		char abc = i;
+		
+		long l = i;
+		//i = l;
+		
+		double d = i;
+		//i = d;
+		
+		float f = 1.23f;
+		//f = d;
+		d = f;
+		
+		int j = 100;
+		
+	}
