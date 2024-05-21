25f8955a017a689c1bf59d1a77fab51868dc72da
Sourabh Tyagi <131552707+SRB-TYAGI@users.noreply.github.com>
2024-05-21 23:48:56 +0530

    Add files via upload

---
+++ Dynamic Programming/Fibo.java
@@ -0,0 +1,17 @@
+public class Fibo{
+    public static int fibDp(int n){
+        int storage[]=new int [n+1];
+        storage[0]=0;
+        storage[1]=1;
+    
+        for (int i=2;i<=n;i++){
+            storage[i]=storage[i-1]+storage[i-2];
+        }
+        return storage[n];
+    }
+
+    public static void main(String args[]){
+        int n=44;
+        System.out.println(fibDp(n));
+    }
+}
